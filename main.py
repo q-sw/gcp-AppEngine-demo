a44a9964d8e208b96ad8d7fe29c92ca793022db5
q-sw <quentin.swiech@gmail.com>
2022-06-07 08:21:55 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from  google.cloud import storage
+from google import auth
+import os
+
+import datetime as dt
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    images = list_bucket_file(os.getenv("thumbnail_bucket"))
+    return render_template('index.html')
+@app.route('/cloud')
+def cloud():
+    images = list_bucket_file(os.getenv("prez_cloud_bucket"))
+    return render_template('slides.html')
+
+def list_bucket_file(bucket_name):
+
+    credentials = auth.default()
+    credentials.refresh(auth.transport.requests.Request())
+
+    expiration = dt.timedelta(hours=1)
+
+    storage_client = storage.Client(credentials=credentials)
+    blobs = storage_client.list_blobs(bucket_name)
+
+
+    slides = {}
+    i = 1
+    for blob in blobs:
+        signed_url = blob.generate_signed_url(expiration=expiration,
+                                              service_account_email=credentials.service_account_email,
+                                              access_token=credentials.token
+                                             )
+        slides[i] = signed_url
+        i += 1
+    return slides
+
+# Uncomment when you want try localy
+# if __name__ == "__main__":
+#    app.run(host='127.0.0.1', port=8080, debug=True)
